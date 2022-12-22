81a9afe18acb7594431e2afc7cde15a2c99be20e
FourteenD <2899385645@qq.com>
2022-12-22 17:02:06 +0800

    vault backup: 2022-12-22 17:02:06
    Affected files:

--- src/views/Demo/Markdown.tsx
+++ src/views/Demo/Markdown.tsx
@@ -9,22 +9,17 @@ export default defineComponent({
   setup() {
     const text = ref("# Hello World");
     const preview = ref<HTMLElement | null>(null);
-    console.log(preview);
-
     const res = computed(() => {
       return marked(text.value);
     });
 
-    console.log(res);
 
     return () => (
       <div class="markdown">
         <div class="editor">
-          <textarea v-model={text.value}></textarea>
+          <textarea value={text.value}></textarea>
         </div>
-        <div class="preview" ref={preview}>
-          <div innerHTML={res.value}></div>
-        </div>
+        <div class="preview" ref={preview}>{text.value}</div>
       </div>
     );
   }

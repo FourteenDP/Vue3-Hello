b399d22ff9a93e617568fdcfd9a9d5d9d95b58ed
FourteenD <2899385645@qq.com>
2022-12-22 17:50:06 +0800

    vault backup: 2022-12-22 17:50:05
    Affected files:

--- src/views/Demo/Markdown.tsx
+++ src/views/Demo/Markdown.tsx
@@ -10,30 +10,17 @@ export default defineComponent({
     title: "markdown编辑器",
   },
   setup() {
-    const text = ref("# Hello World");
+    const editor = ref<HTMLTextAreaElement>();
     const preview = ref<HTMLElement>();
-    const res = computed(() => {
-      return marked(text.value);
-    });
 
-    const update = useDebounceFn(() => {
-      console.log(res.value);
+    console.log(editor, preview);
 
-      if (preview.value) {
-        preview.value.innerHTML = res.value;
-      }
-    }, 1000);
 
 
     return () => (
       <div class="markdown">
         <div class="editor">
-          <textarea value={text.value} onInput={
-            ({ target }) => {
-              text.value = (target as HTMLTextAreaElement).value;
-              update();
-            }
-          } ></textarea>
+          <textarea ref={editor} ></textarea>
         </div>
         <div class={s.preview} ref={preview}></div>
       </div >
